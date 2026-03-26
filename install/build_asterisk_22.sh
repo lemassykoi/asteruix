ac5012fad5d31b6ce8cd121480c05cd856a2019a
Clément PAPPALARDO <16377344+lemassykoi@users.noreply.github.com>
2026-03-26 21:58:50 +0100

    Update build_asterisk_22.sh

--- install/build_asterisk_22.sh
+++ install/build_asterisk_22.sh
@@ -6,43 +6,48 @@ ASTERISK_MAJOR_VERSION="22"
 ASTERISK_VERSION="${ASTERISK_MAJOR_VERSION}-current"
 ASTERISK_URL="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-${ASTERISK_VERSION}.tar.gz"
 
-echo "===== Updating system ====="
-sudo apt update
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: This script must be run as root" >&2
+    echo "Usage: sudo $0 [options]" >&2
+    exit 1
+fi
 
-echo "===== Installing dependencies ====="
-sudo apt install -y build-essential git wget subversion 
-libncurses5-dev libssl-dev libxml2-dev libsqlite3-dev 
-uuid-dev libjansson-dev
+echo "===== 0. Updating system ====="
+apt update -qq
 
-echo "===== Downloading Asterisk ====="
+echo "===== 1. Installing dependencies ====="
+apt install -yqq build-essential git wget subversion libncurses5-dev libssl-dev libxml2-dev libsqlite3-dev uuid-dev libjansson-dev
+
+echo "===== 2. Downloading Asterisk ====="
 cd /usr/src
-sudo wget -q ${ASTERISK_URL}
-sudo tar xvf asterisk-${ASTERISK_VERSION}.tar.gz
+wget -q ${ASTERISK_URL}
+tar xvf asterisk-${ASTERISK_VERSION}.tar.gz
 
 cd asterisk-${ASTERISK_MAJOR_VERSION}.*
 
-echo "===== Installing prerequisites ====="
-sudo contrib/scripts/install_prereq install
+echo "===== 3. Installing prerequisites ====="
+contrib/scripts/install_prereq install
 
-echo "===== Configuring ====="
+echo "===== 4. Configuring ====="
 ./configure
 
-echo "===== Compiling ====="
+echo "===== 5. Compiling ====="
 make -j$(nproc)
 
-echo "===== Installing ====="
-sudo make install
-sudo make samples
-sudo make config
-sudo ldconfig
+echo "===== 6. Installing ====="
+make install
+make samples
+make config
+ldconfig
 
 echo "===== Setting French tones ====="
-sudo sed -i 's/^country=.*/country=fr/' /etc/asterisk/indications.conf
+sed -i 's/^country=.*/country=fr/' /etc/asterisk/indications.conf
 
-echo "===== Starting Asterisk ====="
-sudo systemctl daemon-reexec
-sudo systemctl start asterisk
-sudo systemctl enable asterisk
+echo "===== 7. Starting Asterisk ====="
+systemctl daemon-reexec
+systemctl start asterisk
+systemctl enable asterisk
 
 echo "===== Done ====="
 echo "Connect with: sudo asterisk -rvv"
+exit 0
