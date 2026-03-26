947e034ee3bf118a9b23864f405a80aac4bef957
Clément PAPPALARDO <16377344+lemassykoi@users.noreply.github.com>
2026-03-26 23:58:26 +0100

    Update build_asterisk_22.sh

--- install/build_asterisk_22.sh
+++ install/build_asterisk_22.sh
@@ -51,7 +51,9 @@ echo "===== 4.2.1 Modules ====="
 menuselect/menuselect --enable format_mp3 --enable codec_opus --enable codec_g729a --enable jukebox.agi menuselect.makeopts
 echo "===== 4.2.2 Sounds ====="
 menuselect/menuselect --enable CORE-SOUNDS-FR-G722 --enable CORE-SOUNDS-FR-G729 --enable EXTRA-SOUNDS-FR-G722 --enable EXTRA-SOUNDS-FR-G729 --enable MOH-OPSOUND-G722 --enable MOH-OPSOUND-G729 menuselect.makeopts
-echo "===== 4.2.3 MP3 decoder library ====="
+echo "===== 4.2.3 Disable RADIUS ====="
+menuselect/menuselect --disable cdr_radius --disable cel_radius menuselect.makeopts
+echo "===== 4.2.4 MP3 decoder library ====="
 contrib/scripts/get_mp3_source.sh
 
 echo "===== 5. Compiling ====="
